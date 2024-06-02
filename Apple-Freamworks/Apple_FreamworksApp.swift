32f610abe4c3f9f34ea5ec99ac9f0996a0c5be04
Leon <mrtonmoy12@gmail.com>
2024-06-02 15:41:13 +0600

    Initial Commit

---
+++ Apple-Freamworks/Apple_FreamworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FreamworksApp.swift
+//  Apple-Freamworks
+//
+//  Created by Mahbubur Rashid Leon on 2024-06-02.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FreamworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
